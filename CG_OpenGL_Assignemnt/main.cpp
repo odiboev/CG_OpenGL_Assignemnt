359b925ebb3e9066f7c475522c66c384184a583f
odiboev <odiboev@gmail.com>
2018-10-03 20:27:39 +0800

    basic app testing

---
+++ CG_OpenGL_Assignemnt/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+//Glew
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+//GLFW
+#include <GLFW/glfw3.h>
+
+//Window dimensions
+const GLint WIDTH = 800, HEIGHT = 600;
+
+//The MAIN fubction, from where spplication starts -> Runs game loop
+
+int main() {
+	// Init GLFW
+	glfwInit();
+
+	//Set all required options for glfw 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	//Create a GLWwindow object that we can use for GLFW's functions
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "ComputerGraphics Assinment", nullptr, nullptr);
+
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	if (nullptr == window) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		return EXIT_FAILURE;
+
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//Set this to true so GLEW knows to use a modern approach to retrievingfunction pointers and extensions
+	glewExperimental = GL_TRUE;
+
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (GLEW_OK != glewInit()) {
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Define the viewport dimensions
+	glViewport(0, 0, screenWidth, screenHeight);
+
+	//Game Loop
+	while (!glfwWindowShouldClose(window)) {
+		//Check if any events have been activated (key pressed, mouse moved etc. any call corresponding functions
+		glfwPollEvents();
+
+		//Render
+		//Clear the colorbuffer
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Swap the screen buffers
+		glfwSwapBuffers(window);
+
+	}
+
+	//Terminate GLW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+
+	return EXIT_SUCCESS;
+
+}
